b9b193077e8e0e7e5342210ded9422babb1cf140
Willem Larsen <willemlarsen@gmail.com>
2014-07-10 10:56:44 -0700

    turn minutes into degrees of a circle

---
+++ lib/clock_angle.rb
@@ -0,0 +1,4 @@
+def minutes_to_degrees(minutes)
+	degrees = minutes * 6
+	degrees
+end
--- spec/clock_angle_spec.rb
+++ spec/clock_angle_spec.rb
@@ -2,7 +2,10 @@ require "rspec"
 require "clock_angle.rb"
 
 describe "clock_angle" do 
-	it "does shit" do
-		expect(do_something(shit)).to eq(something)
+	describe "minutes_to_degrees" do
+		it "computes degrees given a certain number of minutes" do
+			expect(minutes_to_degrees(1)).to eq(6)
+		end
 	end
 end
+
